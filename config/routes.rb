0fa3775b99e6a7799fe2530d24e8116b663a971a
TanakaIkuho <sonrisa.enero@gmail.com>
2023-04-17 16:52:06 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-#  root 'posts#index'
-  get "/", to: "posts#index"
+  root 'posts#index'
+  resources :posts do
+    resources :comments
+  end
+  resources :boards
+# get "/", to: "posts#index"
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
